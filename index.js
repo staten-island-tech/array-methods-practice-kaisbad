466c3f35e59db01558128f855690c9473027cb40
kaisbad <158204041+kaisbad@users.noreply.github.com>
2024-10-07 12:19:45 -0400

    a

---
+++ index.js
@@ -0,0 +1,8 @@
+const students = [
+  { name: "Thomas", age: 15, grade: 47 },
+  { name: "Josephine", age: 16, grade: 76 },
+  { name: "Matthew", age: 15, grade: 88 },
+  { name: "Max", age: 16, grade: 65 },
+  { name: "Elisa", age: 15, grade: 100 },
+  { name: "Wilson", age: 16, grade: 20 },
+];
